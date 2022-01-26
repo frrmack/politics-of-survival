00d82cc3dd74e723b262dc6704985ec82cac56be
Irmak Sirer <irmak.sirer@gmail.com>
2022-01-25 21:51:14 -0600

    Initial game element classes

---
+++ elements.py
@@ -0,0 +1,47 @@
+
+
+class Player:
+    # hand cards
+    # influence per module
+    pass
+
+class Card:
+    # name
+    # influence
+    # submodule
+    pass
+
+class Deck:
+    # cards
+    pass
+
+class CardPile:
+    # cards
+    pass
+
+class Die:
+    # tech level
+    pass
+
+class ShipModule:
+    # tech level
+    # submodules
+    # module type
+    # player influences
+    pass
+
+class Spaceship:
+    # modules
+    # dice
+    # cards 
+    pass
+
+class Game:
+    # year
+    # round
+    # spaceship
+    # players
+    # deck
+    # discardpile?
+    pass
+
---
+++ gamesim.py
@@ -0,0 +1,4 @@
+# Irmak Sirer Politics of Survival Design Code
+# Simulations to get a feel for game parameters
+
+from elements import Player, Card, Deck, CardPile, Die, ShipModule, Spaceship, Game
